68c1842871810e22bff8dbfbaeaf574fb7497455
Zul <latiffzul@gmail.com>
2026-04-30 14:18:44 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -286,8 +286,8 @@ const showToast = (msg: string, type: 'success' | 'error' = 'success') => {
   setTimeout(() => setSnackbar((prev) => ({ ...prev, show: false })), 2000);
 };
 // Target date: 8 Ogos 2026
-const targetDate = new Date('2026-01-01T00:00:00'); //test
-// const targetDate = new Date('2026-08-08T00:00:00');
+// const targetDate = new Date('2026-01-01T00:00:00'); //test
+const targetDate = new Date('2026-08-08T00:00:00');
 const isLocked = new Date() < targetDate;
 const now = new Date();
 
